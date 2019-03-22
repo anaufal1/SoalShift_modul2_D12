555c458d7fe75284e8735e17ada365f812ec0f2c
anaufal1 <37364779+anaufal1@users.noreply.github.com>
2019-03-22 22:45:27 +0700

    revisi

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -1,35 +1,49 @@
-#include <stdio.h>
+#include<stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <stdlib.h>
-#include <string.h>
+#include <fcntl.h>
 
-int main()
+int main(){
+
+pid_t child_id;
+  int status;
+
+child_id = fork();
+
+if(child_id == 0)
 {
-	int fd[2], q;
-	pid_t childpid;
-	childpid = fork();
-	if(childpid == 0) execlp("unzip", "unzip", "campur2.zip", NULL);
-	childpid = fork();
-	if(childpid == 0) execlp("touch", "touch", "daftar.txt", NULL);
-	while(wait(&q)>0);
-	pipe(fd);
-	childpid = fork();
-	if(childpid==0)
-	{
-	close(fd[0]);
-	dup2(fd[1], 1);
-	execlp("ls", "ls", "campur2", NULL);
-	}
-	char buffer[256];
-	close(fd[1]);
-	FILE* z = fdopen(fd[0], "r");
-	FILE *daftar = fopen("daftar.txt", "w");
-	int i=0;
-	while (fgets(buffer, sizeof(buffer), z) != NULL) {
-		if (strstr(&buffer[strlen(buffer)-6], ".txt") != NULL)
-			fprintf(daftar, "%s", buffer);
-	}
-	fclose(daftar);
+child_id = fork();
+if (child_id == 0) {
+
+char *argv[5] = {"unzip", "-n","campur2.zip", "*.txt", NULL};
+execv("/usr/bin/unzip", argv);
+  } 
+  else {
+    while ((wait(&status)) > 0);
+int fd[2];
+pipe(fd);
+pid_t pid=fork();
+if(pid==0){
+close(fd[0]);
+dup2(fd[1], 1); 
+char *argv[3] = {"list", "campur2", NULL};
+execv("/bin/ls", argv);
+}   
+else{
+close(fd[1]);       
+dup2(fd[0], 0);
+execlp("tee", "tee","daftar.txt", NULL);                     
+}  
+}
+}
+else
+{
+wait(NULL);
+char *argv[4] = {"unzip", "-n","campur2.zip", NULL};
+execv("/usr/bin/unzip", argv);
+}
+return 0;
 }
